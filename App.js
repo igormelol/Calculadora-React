5b3758daf94a20367f93fb57fcb3e706a9a7bcb2
Igor Melo <igormelolui@gmail.com>
2024-07-06 14:42:18 -0300

    Corrigido alteração de cores

--- App.js
+++ App.js
@@ -55,7 +55,6 @@ export default function App() {
         setCurrentNumber(currentNumber * -1)
         return
     }
-
     setCurrentNumber(currentNumber + buttonPressed)
   }
 
@@ -74,12 +73,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#551A8B' }]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': '#9F79EE' }]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -92,18 +91,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  //Tela acima
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#551A8B"
   },
+  //Números digitados
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
   },
+  //Histórico
   historyText:{
     color: "#7c7c7c",
     fontSize: 20,
@@ -114,16 +116,17 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
+  //Tela de botões
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#68228B',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
-    minHeight: 90,
+    minHeight: 120,
     flex: 2,
   },
   textButton: {
-    color: "#7c7c7c",
+    color: "white",
     fontSize: 20,
   } 
 });
